7cbece6932c636b493183269c2039d51817afea3
Rubens Rodrigues <rubens17rodrigues@gmail.com>
2020-11-11 14:55:09 -0300

    :sparkles: Adiciona arquivo principal.
    Arquivo a ser executado onde estão contidas as regras do jogo.

---
+++ alien_invasion.py
@@ -0,0 +1,24 @@
+import pygame
+
+from settings import Settings
+from ship import Ship
+import game_functions as gf
+
+def run_game():
+    # Inicializa o jogo e cria um objeto para a tela
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width, ai_settings.screen_height))
+    pygame.display.set_caption("Alien Invasion")
+
+    # Cria uma espaçonave
+    ship = Ship(ai_settings, screen)
+
+    # Inicia o laço prinipal do jogo
+    while True:
+        gf.check_events(ship)
+        ship.update()
+        gf.update_screen(ai_settings, screen, ship)
+
+run_game()
