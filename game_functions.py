129725790d426c2800d48734908c09fc8f3c8f64
Rubens Rodrigues <rubens17rodrigues@gmail.com>
2020-12-08 18:24:31 -0300

    :sparkles: Adiciona quantas naves o jogador possui na tela.

--- game_functions.py
+++ game_functions.py
@@ -78,6 +78,7 @@ def reset_game(ai_settings, screen, stats, sb, ship, aliens, bullets):
     sb.prep_score()
     sb.prep_high_score()
     sb.prep_level()
+    sb.prep_ships()
 
     # Esvazia a lista de aliens e de projéteis
     aliens.empty()
@@ -189,11 +190,14 @@ def change_fleet_direction(ai_settings, aliens):
         alien.rect.y += ai_settings.fleet_drop_speed
     ai_settings.fleet_direction *= -1
 
-def ship_hit(ai_settings, stats, screen, ship, aliens, bullets):
+def ship_hit(ai_settings, screen, stats, sb, ship, aliens, bullets):
     """Responde ao fato da nave ter sido atingida por um alien."""
     if stats.ships_left > 0:
         # Decrementa ships_left
         stats.ships_left -= 1
+
+        # Atualiza o painel de pontuações
+        sb.prep_ships()
 
         # Esvazia a lista de aliens e de projéteis
         aliens.empty()
@@ -210,27 +214,27 @@ def ship_hit(ai_settings, stats, screen, ship, aliens, bullets):
         pygame.mouse.set_visible(True)
         stats.game_active = False
 
-def check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets):
+def check_aliens_bottom(ai_settings, screen, stats, sb, ship, aliens, bullets):
     """Verifica se algum alien alcançou a parte inferior da tela."""
     screen_rect = screen.get_rect()
     for alien in aliens.sprites():
         if alien.rect.bottom >= screen_rect.bottom:
             # Trata esse caso igual quando a nave é atingida.
-            ship_hit(ai_settings, stats, screen, ship, aliens, bullets)
+            ship_hit(ai_settings, screen, stats, sb,  ship, aliens, bullets)
             break
 
 
-def update_aliens(ai_settings, stats, screen, ship, aliens, bullets):
+def update_aliens(ai_settings, screen, stats, sb, ship, aliens, bullets):
     """Atualiza as posições de todos os aliens."""
     check_fleet_edges(ai_settings, aliens)
     aliens.update()
 
     # Verifica colisão da nave e dos aliens.
     if pygame.sprite.spritecollideany(ship, aliens):
-        ship_hit(ai_settings, stats, screen, ship, aliens, bullets)
+        ship_hit(ai_settings, screen, stats, sb, ship, aliens, bullets)
 
     # Verifica se há algum alien que atingiu a parte inferior da tela
-    check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets)
+    check_aliens_bottom(ai_settings, screen, stats, sb, ship, aliens, bullets)
 
 def check_high_score(stats, sb):
     """Verifica se há uma nova pontuação máxima."""
